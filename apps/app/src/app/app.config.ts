21767e29b0a15740fa1fb52c8286964225dcb7d9
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-05-07 13:51:01 +0200

    app initializier

--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/common/http';
 import {
   ApplicationConfig,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import {
@@ -16,7 +17,9 @@ import {
   withComponentInputBinding,
   withInMemoryScrolling,
 } from '@angular/router';
+import { filter, first } from 'rxjs';
 import { appRoutes } from './app.routes';
+import { injectAuthClient } from './auth/auth-client';
 import { cookiesInterceptor } from './auth/cookies.interceptor';
 import { loggingInterceptor } from './auth/logging.interceptor';
 
@@ -37,13 +40,13 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([loggingInterceptor, cookiesInterceptor]),
     ),
     // used to prevent flicker while better auth state is being loaded on app start, by waiting for first non pending session state before app initialization is completed
-    // provideAppInitializer(() => {
-    //   const auth = injectAuthClient();
-    //   return auth.useSession().pipe(
-    //     filter((s) => !s.isPending),
-    //     // app initializer observable must complete, so using first non pending session state to know when better auth is ready
-    //     first(),
-    //   );
-    // }),
+    provideAppInitializer(() => {
+      const auth = injectAuthClient();
+      return auth.useSession().pipe(
+        filter((s) => !s.isPending),
+        // app initializer observable must complete, so using first non pending session state to know when better auth is ready
+        first(),
+      );
+    }),
   ],
 };
