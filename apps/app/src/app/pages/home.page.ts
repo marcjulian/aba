fb5279b7fae1c8e8a0e2aa34d7346c10e482a726
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-05-07 10:54:25 +0200

    call health endpoint and show data on home page

--- apps/app/src/app/pages/home.page.ts
+++ apps/app/src/app/pages/home.page.ts
@@ -1,4 +1,6 @@
+import { httpResource } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 import { BaseLayout } from '../layouts/base.layout';
 
 @Component({
@@ -7,8 +9,22 @@ import { BaseLayout } from '../layouts/base.layout';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <app-base-layout>
-      <h1>Home Page</h1>
+      <div class="mx-auto max-w-(--breakpoint-lg) px-4">
+        <h1>Home Page</h1>
+        <p>
+          SSR rendered at:
+          @if (timeResource.isLoading()) {
+            loading...
+          } @else if (timeResource.hasValue()) {
+            <strong>{{ timeResource.value().timestamp }}</strong>
+          }
+        </p>
+      </div>
     </app-base-layout>
   `,
 })
-export class HomePage {}
+export class HomePage {
+  timeResource = httpResource<{ status: string; timestamp: string }>(
+    () => `${environment.apiUrl}/api/health`,
+  );
+}
