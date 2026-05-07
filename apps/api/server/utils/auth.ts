3abc73bb55bae6bc7e6fbf4db36e41198db0531e
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-05-07 12:32:07 +0200

    comment out baseUrl for prod

--- apps/api/server/utils/auth.ts
+++ apps/api/server/utils/auth.ts
@@ -3,7 +3,7 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { prisma } from './prisma';
 
 export const auth = betterAuth({
-  baseURL: process.env['BETTER_AUTH_URL'],
+  // baseURL: process.env['BETTER_AUTH_URL'],
   secret: process.env['BETTER_AUTH_SECRET'],
   trustedOrigins: [process.env['APP_URL']!],
   advanced: {
