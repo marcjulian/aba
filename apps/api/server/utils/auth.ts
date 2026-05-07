05bf9cfae1c3414b337a178e34395f6410cc8c47
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-05-07 16:54:29 +0200

    add root domain env

--- apps/api/server/utils/auth.ts
+++ apps/api/server/utils/auth.ts
@@ -3,14 +3,14 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { prisma } from './prisma';
 
 export const auth = betterAuth({
-  // baseURL: process.env['BETTER_AUTH_URL'],
+  baseURL: process.env['BETTER_AUTH_URL'],
   secret: process.env['BETTER_AUTH_SECRET'],
   trustedOrigins: [process.env['APP_URL']!],
   advanced: {
-    // trustedProxyHeaders: true,
+    // enable cross subdomain cookies for auth sessions, when api and app are on different subdomains of the same root domain
     crossSubDomainCookies: {
       enabled: true,
-      domain: 'aba.marcjulian.de',
+      domain: process.env['ROOT_DOMAIN'],
     },
   },
   database: prismaAdapter(prisma, {
