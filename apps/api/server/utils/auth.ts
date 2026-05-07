9c18aa28161f08ae6b8acbe54c6a183dd8e3a2b7
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-05-07 12:30:37 +0200

    try out trustedProxyHeaders

--- apps/api/server/utils/auth.ts
+++ apps/api/server/utils/auth.ts
@@ -6,6 +6,9 @@ export const auth = betterAuth({
   baseURL: process.env['BETTER_AUTH_URL'],
   secret: process.env['BETTER_AUTH_SECRET'],
   trustedOrigins: [process.env['APP_URL']!],
+  advanced: {
+    trustedProxyHeaders: true,
+  },
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
