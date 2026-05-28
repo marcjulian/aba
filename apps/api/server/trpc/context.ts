d3917b9ba7f5bdcf17d1bf4462c24725fc4ce23f
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-05-28 11:09:40 +0200

    cleanup createContext

--- apps/api/server/trpc/context.ts
+++ apps/api/server/trpc/context.ts
@@ -1,16 +1,10 @@
 import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
-import { auth } from '../utils/auth';
 
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
 export const createContext = async ({ req }: FetchCreateContextFnOptions) => {
-  const headers = req.headers;
-  const session = await auth.api.getSession({
-    headers: headers,
-  });
-
   return {
     req,
   };
