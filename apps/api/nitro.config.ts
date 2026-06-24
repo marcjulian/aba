16ec0ba144b9f827adabcdeec13857d35f448dc1
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-06-24 10:46:27 +0200

    update nitro output dir

--- apps/api/nitro.config.ts
+++ apps/api/nitro.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'nitro';
 
 export default defineConfig({
   serverDir: './server',
+  output: {
+    dir: '../../dist/apps/api',
+  },
   routes: {
     '/api/auth/**': './server/utils/auth-route.ts',
     '/api/trpc/**': './server/trpc/trpc-handler.ts',
